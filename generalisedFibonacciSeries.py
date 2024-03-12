1969e672228dbdbca86a6f5a46cf7ff0390a97a2
Akshat Pathak <akshat.pathak03@gmail.com>
2024-03-12 23:20:36 +0530

    Update generalisedFibonacciSeries.py
    My solution was wrong for other test cases.
    I found a solution which demonstrates the use inner function

--- generalisedFibonacciSeries.py
+++ generalisedFibonacciSeries.py
@@ -1,25 +1,21 @@
 import numpy as np
 
 class Solution:
-    #My Solution using recursion
+    #My Solution using recursion (Incorrect )
     def genFibNum(self, a, b, c, n, m):
         # code here 
         ans =  self.gen(n-1, m) * a + self.gen(n-2, m) * b + c
         return ans % m
         
     def gen(self, n, m):
-        if n == 1:
-            return 1 % m
-        elif n == 2:
-            return 1 % m
-        elif n == 0:
-            return 0
+        if n == 1 or n == 2 :
+            return 1 
         else:
             a = 1
             b = 1
             for _ in range(n - 2):
                 a, b = b, (a + b) % m
-            return b
+            return b 
     #Solution using Matrices    
     def genFibNum2(self, a, b, c, n, m):
         if n <= 2:
@@ -41,7 +37,14 @@ class Solution:
             gen_fib_matrix = np.matmul(gen_fib_matrix, gen_fib_matrix) % m
             n >>= 1
         return sum(res[0]) % m
-
+    # Solution using inner function
+    def genFibNum3(self,a,b,c,n,m):
+        def g(n):
+            if n == 1 or n == 2:
+                return 1
+            else:
+                return a *g(n-1) + b * g(n-2) + c
+        return g(n)%m
 # Example usage:
 ob = Solution()
 a = 3
@@ -49,13 +52,15 @@ b = 3
 c = 3
 n = 3
 m = 5
-print(ob.genFibNum(a, b, c, n, m))  # Output: 4
-print(ob.genFibNum2(a, b, c, n, m))
+# print(ob.genFibNum(a, b, c, n, m))  # Output: 4
+# print(ob.genFibNum2(a, b, c, n, m))
+# print(ob.genFibNum3(a,b,c,n,m))
 
 a = 2
 b = 2
 c = 2
-n = 4
+n = 5
 m = 100
 print(ob.genFibNum(a, b, c, n, m))  # Output: 16
 print(ob.genFibNum2(a, b, c, n, m))
+print(ob.genFibNum3(a,b,c,n,m))
