322c836ebd8236f01884553d550e4016e1f0e6fa
Akshat Pathak <akshat.pathak03@gmail.com>
2024-03-19 12:28:09 +0530

    create .py files
    easy problems

---
+++ forLoops2.py
@@ -0,0 +1,10 @@
+def stringJumper(str):
+    for i in range(0,len(str),2): ## from 0 to length of str and skip 2
+        print(str[i], end="") ##printing character and separating characters by nothing
+
+
+def stringJumper2(str):
+    print(str[::2])
+
+
+exstr = "AinsdfoioWNOZasdaisnOINonONINOjnONONoNNCO"
