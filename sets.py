f5f4a9f0d7bf251c6c10cf5e33fd004524bd4b8b
Akshat Pathak <akshat.pathak03@gmail.com>
2024-03-25 09:12:55 +0530

    create .py files
    Easy python problems on sets

---
+++ sets.py
@@ -0,0 +1,11 @@
+def insert(S, element):
+    S.add(element)
+# Function to remove element from set
+# No return and nothing to print
+def remove_from_set(S, element):
+    S.remove(element)
+# Function to find sum of elements in set
+# return value should be there as sum
+def sum_set(S):
+    ans = sum(S)
+    return ans
