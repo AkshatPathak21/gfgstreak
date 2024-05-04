7b754ca5f9ffcbec161dedf98510f1cd75371a96
Akshat Pathak <akshat.pathak03@gmail.com>
2024-05-04 10:40:06 +0530

    Create constructTreeInorderPostorder.py
    POTD 61

---
+++ constructTreeInorderPostorder.py
@@ -0,0 +1,15 @@
+class Node:
+            def __init__(self, data):
+                self.data = data
+                self.left = self.right = None
+                
+class Solution:
+    def buildTree(self, In, post, n):
+        # your code here
+        if not In or not post:
+            return 
+        index = In.index(post[-1])
+        root = Node(post[-1])
+        root.left = self.buildTree(In[0:index],post[0:index],n)
+        root.right = self.buildTree(In[index+1:],post[index:-1],n)
+        return root
