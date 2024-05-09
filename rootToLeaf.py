284e809500ae25916797d6f6769e0ae8c70ccea5
Akshat Pathak <akshat.pathak03@gmail.com>
2024-05-09 10:32:50 +0530

    Update rootToLeaf.py

--- rootToLeaf.py
+++ rootToLeaf.py
@@ -1,8 +1,15 @@
+
+from typing import Optional
+from collections import deque
+
+from typing import List
+
 class Node:
     def _init_(self,val):
         self.data = val
         self.left = None
         self.right = None
+
 class Solution:
     def dfs(self, node, curr_path, ans):
         if not node: return
